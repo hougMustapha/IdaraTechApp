023f1b37526ebb9c4d709cd0aa6b5ad7f7297414
Mustapha <mustapha.ligsoft@gmail.com>
2023-12-07 21:42:50 +0100

    03, Client Project setup with registration

--- IdaraTechApi/DTOs/Account/LoginDto.cs
+++ IdaraTechApi/DTOs/Account/LoginDto.cs
@@ -4,7 +4,7 @@ namespace IdaraTech_Admin.DTOs.Account
 {
     public class LoginDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
